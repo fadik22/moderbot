db50dd892029c400445d1af28fef5a39c97f3886
fadik22 <salimoviskander5@gmail.com>
2025-01-26 18:54:36 +0300

    Add files via upload

--- logic.py
+++ logic.py
@@ -13,6 +13,9 @@ class Pokemon:
         self.name = self.get_name()
         self.types = self.get_types()
         self.weightheight = self.get_weight_height()
+        self.level = self.get_level()
+        self.hp = randint(1,100)
+        self.power = randint(1,100)
         Pokemon.pokemons[pokemon_trainer] = self
     # Метод для получения картинки покемона через API
     def get_img(self):
@@ -52,12 +55,54 @@ class Pokemon:
             data = response.json()
             return data['height'], data['weight']
         return None, None
+
+    def get_level(self):
+        url = f'https://pokeapi.co/api/v2/pokemon/{self.pokemon_number}'
+        response = requests.get(url)
+        if response.status_code == 200:
+            data = response.json()
+            return data['base_experience']
+        return None
     def info(self):
-        return f"Имя твоего покеомона: {self.name}, его тип: {', '.join(self.types)}, вес: {self.weightheight[1]}, высота: {self.weightheight[0]}"
+        return f"Имя твоего покеомона: {self.name}, его тип: {', '.join(self.types)}, вес: {self.weightheight[1]}, высота: {self.weightheight[0]}, уровень: {self.get_level()}, hp: {self.hp}, power: {self.power}"
 
     # Метод класса для получения картинки покемона
     def show_img(self):
         return self.img
 
 
+class Wizard(Pokemon):
+    """Подкласс Pokemon, представляющий покемона-волшебника."""
 
+    def info(self):
+        """Возвращает информацию о покемоне-волшебнике."""
+        return "У тебя покемон-волшебник \n\n" + super().info()
+
+class Fighter(Pokemon):
+    """Подкласс Pokemon, представляющий покемона-бойца."""
+
+    def attack(self, enemy):
+        """Атака с использованием супер-удара."""
+        super_power = randint(5, 15)  # Генерируем дополнительную силу атаки
+        self.power += super_power  # Увеличиваем силу атаки перед ударом
+        result = super().attack(enemy)  # Вызываем стандартную атаку
+        self.power -= super_power  # Возвращаем силу атаки к изначальному значению
+        return result + f"\nБоец применил супер-атаку силой: {super_power} "
+
+    def info(self):
+        """Возвращает информацию о покемоне-бойце."""
+        return "У тебя покемон-боец \n\n" + super().info()
+
+# Создаём двух покемонов: волшебника и бойца
+wizard = Wizard("username1")
+fighter = Fighter("username2")
+
+# Выводим информацию о покемонах
+print(wizard.info())
+print("#" * 10)
+print(fighter.info())
+print("#" * 10)
+
+# Проводим сражение между покемонами
+print(wizard.attack(fighter))
+print(fighter.attack(wizard))
--- main.py
+++ main.py
@@ -14,8 +14,10 @@ def go(message):
         bot.send_photo(message.chat.id, pokemon.show_img())
         bot.send_types(message.chat.id, str(pokemon.show_types()))
         bot.send_weight_height(message.chat.id, str(pokemon.show_weight_height()))
+        bot.send_level(message.chat.id, str(pokemon.show_level()))
     else:
         bot.reply_to(message, "Ты уже создал себе покемона")
+
 
 @bot.message_handler(commands=['types'])
 def types(message):
@@ -24,7 +26,10 @@ def types(message):
 @bot.message_handler(commands=['help'])
 def help(message):
     bot.reply_to(message, "Напиши /go для создания покемона")
+
+@bot.message_handler(commands=['level'])
+def level(message):
+    bot.reply_to(message, "[+] Вы улучшили своего покемона")
+
+
 bot.infinity_polling(none_stop=True)
-
-
-
